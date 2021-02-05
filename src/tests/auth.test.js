240508a439d7862e003f9b8808b633d82b60724c
Oscar Islas <oislasreyes@gmail.com>
2021-02-04 21:43:39 -0600

    Corrección en los 3 ultimos tests

--- src/tests/auth.test.js
+++ src/tests/auth.test.js
@@ -1,129 +1,118 @@
 import request from "supertest";
 import app from "../index";
-import {Users} from "../models/";
+import { Users } from "../models/";
 
 const user = {
-    firstName: "Gustavo",
-    lastName: "Hernandez",
-    email: "gus91@gmail.com",
-    password: "gus12345&"
-}
+  firstName: "Gustavo",
+  lastName: "Hernandez",
+  email: "gus91@gmail.com",
+  password: "gus12345&",
+};
 
 const userLogin = {
-    email: "gus91@gmail.com",
-    password: "gus12345&"
-}
+  email: "gus91@gmail.com",
+  password: "gus12345&",
+};
 
 const invalidUserLogin = {
-    email: "gus91@gmail.com",
-    password: "gus123456&"
-}
+  email: "gus91@gmail.com",
+  password: "gus123456&",
+};
 
 const invalidUser = {
-    firstName: "",
-    lastName: "",
-    email: "gus02@gmail.com",
-    password: "gus12345&"
-}
+  firstName: "",
+  lastName: "",
+  email: "gus02@gmail.com",
+  password: "gus12345&",
+};
 
 let token = "";
-let invalidToken = "2yhasidi12691djsdkabao"
+let invalidToken = "2yhasidi12691djsdkabao";
 let userID = 0;
 
 describe("Pruebas Usuarios", () => {
+  it("Agregar al usuario Gustavo", async () => {
+    const results = await request(app).post("/signin").send(user);
+    expect(results.statusCode).toEqual(201);
+    expect(results.body).toHaveProperty("email", user.email);
+    userID = results.body.id;
+  });
 
-    it("Agregar al usuario Gustavo", async () => {
-        const results = await request(app)
-        .post("/signin")
-        .send(user);
-        expect(results.statusCode).toEqual(201);
-        expect(results.body).toHaveProperty('email', user.email);
-        userID = results.id;
-    });
+  it("Registro fallido al agregar un usuario con el mismo correo", async () => {
+    const results = await request(app).post("/signin").send(user);
+    expect(results.statusCode).toBeGreaterThanOrEqual(400);
+    expect(results.body).toHaveProperty("message");
+  });
 
-    it("Registro fallido al agregar un usuario con el mismo correo", async () => {
-        const results = await request(app)
-        .post("/signin")
-        .send(user);
-        expect(results.statusCode).toBeGreaterThanOrEqual(400);
-        expect(results.body).toHaveProperty('message');
-    });
-
-    it("Registro fallido al agregar un usuario sin nombre y apellido", async () => {
-        const results = await request(app)
-        .post("/signin")
-        .send(invalidUser);
-        expect(results.statusCode).toBeGreaterThanOrEqual(400);
-        expect(results.body).toHaveProperty('message');
-    });
+  it("Registro fallido al agregar un usuario sin nombre y apellido", async () => {
+    const results = await request(app).post("/signin").send(invalidUser);
+    expect(results.statusCode).toBeGreaterThanOrEqual(400);
+    expect(results.body).toHaveProperty("message");
+  });
 });
 
 describe("Autenticacion", () => {
-    it("Iniciar sesión", async () => {
-        const results = await request(app)
-        .post("/login")
-        .send(userLogin);
-        expect(results.statusCode).toBeGreaterThanOrEqual(200);
-        expect(results.body).toHaveProperty('message');
-    });
+  it("Iniciar sesión", async () => {
+    const results = await request(app).post("/login").send(userLogin);
+    expect(results.statusCode).toBeGreaterThanOrEqual(200);
+    expect(results.body).toHaveProperty("message");
+  });
 
-    it("Inicio de sesión con credenciales incorrectas", async () => {
-        const results = await request(app)
-        .post("/login")
-        .send(invalidUserLogin);
-        expect(results.statusCode).toBeGreaterThanOrEqual(401);
-        expect(results.body).toHaveProperty('message');
-    });
+  it("Inicio de sesión con credenciales incorrectas", async () => {
+    const results = await request(app).post("/login").send(invalidUserLogin);
+    expect(results.statusCode).toBeGreaterThanOrEqual(401);
+    expect(results.body).toHaveProperty("message");
+  });
 
-    it("Inicio de sesión y respuesta de un token JWT", async () => {
-        const results = await request(app)
-        .post("/login")
-        .send(userLogin);
-        expect(results.statusCode).toBeGreaterThanOrEqual(200);
-        expect(results.body).toHaveProperty('token');
-        token = results.body.token;
-    });
+  it("Inicio de sesión y respuesta de un token JWT", async () => {
+    const results = await request(app).post("/login").send(userLogin);
+    expect(results.statusCode).toBeGreaterThanOrEqual(200);
+    expect(results.body).toHaveProperty("token");
+    token = results.body.token;
+  });
 });
 
 describe("Acceso a rutas protegidas | Autorización | (Opcional)", () => {
-    it("Obtener un usuario por ID con token de autorización", async () => {
-        const results = await request(app)
-        .get("/users/" + userID)
-        .set('Authorization', 'Bearer ' + token);
-        expect(results.statusCode).toBeGreaterThanOrEqual(200);
-        expect(results.body).toHaveProperty('lastName', 'Hernandez');
-    });
+  it("Obtener un usuario por ID con token de autorización", async () => {
+    const results = await request(app)
+      .get("/users/" + userID)
+      .set("Authorization", "Bearer " + token);
+    expect(results.statusCode).toBeGreaterThanOrEqual(200);
+    expect(results.body).toHaveProperty("lastName", "Hernandez");
+  });
 
-    it("Obtener todos los usuarios con token de autorización", async () => {
-        const results = await request(app)
-        .get("/users")
-        .set('Authorization', 'Bearer ' + token);
-        expect(results.statusCode).toBeGreaterThanOrEqual(200);
-        expect(typeof results.body).toBe('array');
-        expect(results.body).toEqual(
-            expect.arrayContaining([
-              expect.objectContaining(user),
-            ])
-        );
-    });
+  it("Obtener todos los usuarios con token de autorización", async () => {
+    const results = await request(app)
+      .get("/users")
+      .set("Authorization", "Bearer " + token);
+    expect(results.statusCode).toBeGreaterThanOrEqual(200);
+    expect(results.body).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({
+          email: user.email,
+          lastName: user.lastName,
+          firstName: user.firstName,
+        }),
+      ])
+    );
+  });
 
-    it("Obtener todos los usuarios con token de autorización invalido", async () => {
-        const results = await request(app)
-        .get("/users")
-        .set('Authorization', 'Bearer ' + invalidToken);
-        expect(results.statusCode).toBeGreaterThanOrEqual(401);
-        expect(results.body).toHaveProperty('message');
-    });
+  it("Obtener todos los usuarios con token de autorización invalido", async () => {
+    const results = await request(app)
+      .get("/users")
+      .set("Authorization", "Bearer " + invalidToken);
+    expect(results.statusCode).toBeGreaterThanOrEqual(401);
+    expect(results.body).toHaveProperty("message");
+  });
 });
 
-beforeAll(done => {
-    done();
+beforeAll((done) => {
+  done();
 });
-
 
 afterAll(async (done) => {
-    //Borrar todos los datos de la base de datos
-    await Users.destroy({where: {email: "gus91@gmail.com"}});
-    await Users.destroy({where: {email: "gus02@gmail.com"}});
-    done();
+  //Borrar todos los datos de la base de datos
+  await Users.destroy({ where: { email: user.email } });
+  await Users.destroy({ where: { email: invalidUser.email } });
+  done();
 });
